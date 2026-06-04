7c995418c672c21e1b5690cfbc043f64a8bf4ea1
ferdinando <ferdinando.giordano@egomedia.it>
2026-06-04 17:13:48 +0200

    fix(blog): istantanee 4 colonne aspect 3:4 con 8 immagini (Figma 671:8546) v0.9.7

--- villasg-child/patterns/blog-istantanee.php
+++ villasg-child/patterns/blog-istantanee.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Title: Blog – Istantanee (5 colonne)
+ * Title: Blog – Istantanee (4 colonne, 8 immagini)
  * Slug: villasg-child/blog-istantanee
  * Categories: villasg-blog
  * Description: Galleria 5 immagini full-bleed con titolo "Istantanee" e CTA "Esplora la Gallery →".
@@ -14,13 +14,16 @@ $vsg_theme = get_stylesheet_directory_uri();
 	<h2 class="wp-block-heading has-large-font-size" style="font-style:normal;font-weight:400">Istantanee</h2>
 	<!-- /wp:heading -->
 
-	<!-- wp:gallery {"columns":5,"linkTo":"none","sizeSlug":"large","align":"wide","className":"vsg-blog-istantanee__gallery"} -->
-	<figure class="wp-block-gallery alignwide has-nested-images columns-5 is-cropped vsg-blog-istantanee__gallery">
+	<!-- wp:gallery {"columns":4,"linkTo":"none","sizeSlug":"large","align":"wide","className":"vsg-blog-istantanee__gallery"} -->
+	<figure class="wp-block-gallery alignwide has-nested-images columns-4 is-cropped vsg-blog-istantanee__gallery">
 		<!-- wp:image {"sizeSlug":"large"} --><figure class="wp-block-image size-large"><img src="<?php echo esc_url( $vsg_theme ); ?>/assets/images/gallery-1.jpg" alt=""/></figure><!-- /wp:image -->
 		<!-- wp:image {"sizeSlug":"large"} --><figure class="wp-block-image size-large"><img src="<?php echo esc_url( $vsg_theme ); ?>/assets/images/gallery-2.jpg" alt=""/></figure><!-- /wp:image -->
 		<!-- wp:image {"sizeSlug":"large"} --><figure class="wp-block-image size-large"><img src="<?php echo esc_url( $vsg_theme ); ?>/assets/images/gallery-3.jpg" alt=""/></figure><!-- /wp:image -->
 		<!-- wp:image {"sizeSlug":"large"} --><figure class="wp-block-image size-large"><img src="<?php echo esc_url( $vsg_theme ); ?>/assets/images/gallery-4.jpg" alt=""/></figure><!-- /wp:image -->
 		<!-- wp:image {"sizeSlug":"large"} --><figure class="wp-block-image size-large"><img src="<?php echo esc_url( $vsg_theme ); ?>/assets/images/gallery-5.jpg" alt=""/></figure><!-- /wp:image -->
+		<!-- wp:image {"sizeSlug":"large"} --><figure class="wp-block-image size-large"><img src="<?php echo esc_url( $vsg_theme ); ?>/assets/images/gallery-6.jpg" alt=""/></figure><!-- /wp:image -->
+		<!-- wp:image {"sizeSlug":"large"} --><figure class="wp-block-image size-large"><img src="<?php echo esc_url( $vsg_theme ); ?>/assets/images/gallery-7.jpg" alt=""/></figure><!-- /wp:image -->
+		<!-- wp:image {"sizeSlug":"large"} --><figure class="wp-block-image size-large"><img src="<?php echo esc_url( $vsg_theme ); ?>/assets/images/gallery-8.jpg" alt=""/></figure><!-- /wp:image -->
 	</figure>
 	<!-- /wp:gallery -->
 
