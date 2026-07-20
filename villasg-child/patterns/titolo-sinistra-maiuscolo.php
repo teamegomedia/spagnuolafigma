da215c9c0d14a62675f86649ec9af1fe5e5c0077
ferdinando <ferdinando.giordano@egomedia.it>
2026-07-20 15:43:27 +0200

    Pattern: Titolo centrato e Titolo SX con titolino maiuscolo (villasg-sections) + bump 0.10.4

---
+++ villasg-child/patterns/titolo-sinistra-maiuscolo.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Title: Titolo Allineato SX con titolino maiuscolo
+ * Slug: villasg-child/titolo-sinistra-maiuscolo
+ * Categories: villasg-sections
+ * Description: Blocco intestazione allineato a sinistra: titolino maiuscolo (overline SeaBlue) sopra un titolo con parola in corsivo. Da export editor WordPress.
+ */
+?>
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"0","bottom":"var:preset|spacing|20","left":"0","right":"0"},"margin":{"top":"0","bottom":"0"},"blockGap":"var:preset|spacing|20"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group" style="margin-top:0;margin-bottom:0;padding-top:0;padding-right:0;padding-bottom:var(--wp--preset--spacing--20);padding-left:0"><!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"500","textTransform":"uppercase","letterSpacing":"0.2em"},"elements":{"link":{"color":{"text":"var:preset|color|seablue1"}}}},"textColor":"seablue1","fontSize":"small-caps"} -->
+<p class="has-seablue-1-color has-text-color has-link-color has-small-caps-font-size" style="font-style:normal;font-weight:500;letter-spacing:0.2em;text-transform:uppercase">Le anime della dimora</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:heading {"fontSize":"x-large"} -->
+<h2 class="wp-block-heading has-x-large-font-size">Scenografie per la <em>vostra </em>storia</h2>
+<!-- /wp:heading --></div>
+<!-- /wp:group -->
